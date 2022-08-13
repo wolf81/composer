2c2785580b9eda8755de169590bb6452e8e5f43e
Wolfgang Schreurs <wolfgang@wolftrail.net>
2022-08-13 21:46:37 +0700

    Update example layout

--- examples/example1.lua
+++ examples/example1.lua
@@ -15,7 +15,7 @@ end
 local function makeImageButton(image_path)
     return ImageButton { 
         image = image_path,
-        corner_radius = 30,
+        corner_radius = 0,
     }
 end
 
@@ -38,39 +38,50 @@ end
 local function makeProgress()
     return Progress { corner_radius = 3, value = 0.1 }
 end
+--[[
+if row no height:
+    if row no children: fill
+    if row children: calc total height of children & make row same size
+else use row height
 
+if col no height
+    if col no children: fill
+    if col children: calc total width of children and make col same size
+else use col height
+
+]]
 return Layout(Margin(10), Rows(Spacing(10), {
     Row(),
     Row(40, Cols(Spacing(10), {
         Col(),
-        Col(150, makeButton('BUTTON')),
+        Col(100, makeButton('BUTTON')),
         Col(),
     })),
     Row(40, Cols(Spacing(10), {
         Col(),
-        Col(150, makeLabel('Label')),
+        Col(20, makeCheckbox()),
         Col(),
     })),    
     Row(40, Cols(Spacing(10), {
         Col(),
-        Col(150, makeCheckbox()),
+        Col(100, makeLabel('Label')),
         Col(),
     })),    
     Row(40, Cols(Spacing(10), {
         Col(),
-        Col(makeImageButton('assets/crossbow.png')),
+        Col(makeImageButton('assets/sparkling-sabre.png')),
         Col(),
-    })),
-    Row(40, Cols(Spacing(10), {
+    })),    
+    Row(40, Cols {
         Col(),
-        Col(makeSlider()),
+        Col(210, makeSlider()),
         Col(),
-    })),
-    Row(40, Cols(Spacing(10), {
+    }),
+    Row(40, Cols {
         Col(),
-        Col(makeInput('blaat')),
+        Col(210, makeInput('blaat')),
         Col(),
-    })),
+    }),
     Row(40, Cols {
         Col(),
         Col(210, makeProgress()),
