dc431f5f608d7f4d8b7808c9944260b5e92313b8
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2022-08-19 11:59:10 +0700

    Code clean-up

--- examples/example4.lua
+++ examples/example4.lua
@@ -1,9 +1,9 @@
 local function makeButton(text)
-    return Button { 
+    return Button(50, { 
         text = text, 
         font = { 'assets/agricola-rough.otf', 24 }, 
         corner_radius = 10,
-    }
+    })
 end
 
 local function makeLabel(text)
@@ -14,15 +14,22 @@ local function makeLabel(text)
     }
 end
 
-return Layout(HStack {
+return Layout(VStack(Spacing(20), {
+    Space(),
+    HStack({
+        Space(),
+        makeLabel('MY AWESOME GAME'),
+        Space(),
+    }),
+    HStack({
+        Space(),
+        VStack(300, Spacing(10), {
+            makeButton('NEW GAME'),
+            makeButton('CONTINUE'),
+            makeButton('SETTINGS'),
+            makeButton('QUIT'),        
+        }),
+        Space(),        
+    }),
 	Space(),
-	VStack(300, Spacing(8), {
-		makeLabel('MY AWESOME GAME'),
-		makeButton('NEW GAME'),
-		makeButton('CONTINUE'),
-		makeButton('SETTINGS'),
-		makeButton('QUIT'),
-		Space(),
-	}),
-	Space(),
-})
+}))
